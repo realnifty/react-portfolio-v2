a585d036aae295b9f86715513b98f93c47bef77f
gordo <rogelioalbertopedraza@gmail.com>
2022-08-09 12:31:19 -0500

    animated responsive sidebar

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -1,27 +1,38 @@
+import React, { useState } from "react";
+
+import Hamburger from "hamburger-react";
+import Sidebar from "../Sidebar";
 
 
 function Nav() {
-    const sections = [
-        {name: "About", link: "#about"},
-        {name: "Skills", link: "#skills"},
-        {name: "Projects", link: "#projects"},
-        {name: "Contact", link: "#contact"},
+  const sections = [
+    { name: "About", link: "#about" },
+    { name: "Skills", link: "#skills" },
+    { name: "Projects", link: "#projects" },
+    { name: "Contact", link: "#contact" },
     ];
-    return (
-        <>
-            <nav>
-                <h1 className="font-bebas text-orange-600">R.P.</h1>
-                <div className="text-stone-400">
-                    <ul>
-                        {sections.map((section) => (
-                            <li><a href={section.link}>{section.name}</a></li>
-                        ))}
-                    </ul>
-                    <button>Resume</button>
-                </div>
-            </nav>
-        </>
-    )
+    const [showSidebar, setShowSidebar] = useState(false);
+  return (
+    <>
+    <Sidebar showSidebar={showSidebar}/>
+      <header className="flex justify-between">
+        <div className="flex z-30 justify-between items-center w-full m-5">
+            <h1 className="font-bebas text-3xl text-orange-400">R.P.</h1>
+            <Hamburger color="#fb923c" toggled={showSidebar} toggle={setShowSidebar}/>
+        </div>
+        <nav className="text-stone-400 hidden">
+          <ul>
+            {sections.map((section) => (
+              <li key={section.name}>
+                <a href={section.link}>{section.name}</a>
+              </li>
+            ))}
+          </ul>
+          <button>Resume</button>
+        </nav>
+      </header>
+    </>
+  );
 }
 
 export default Nav;
---
+++ src/components/Sidebar/index.js
@@ -0,0 +1,28 @@
+import React from "react";
+
+function Sidebar(props) {
+    const {showSidebar} = props;
+
+    const sections = [
+      { name: "About", link: "#about" },
+      { name: "Skills", link: "#skills" },
+      { name: "Projects", link: "#projects" },
+      { name: "Contact", link: "#contact" },
+    ];
+    return (
+        <>
+            <aside className={` ${showSidebar ? 'translate-x-0' : 'translate-x-full'} ease-in-out duration-200 top-0 right-0 fixed w-3/5 text-center text-stone-400 bg-stone-800 h-screen`}>
+                <ul className="mt-20 mb-5">
+                {sections.map((section) => (
+                    <li key={section.name} className="my-3 text-lg">
+                    <a href={section.link}>{section.name}</a>
+                    </li>
+                ))}
+                </ul>
+                <button className="outline outline-1 outline-orange-400 p-2 rounded-md"><span className="text-orange-400">Resume</span></button>
+            </aside>
+        </>
+    );
+}
+
+export default Sidebar;
