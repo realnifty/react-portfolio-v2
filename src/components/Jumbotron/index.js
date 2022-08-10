0854bac1b83e7e3bd5436245ac2c2f260cd726c3
gordo <rogelioalbertopedraza@gmail.com>
2022-08-10 08:38:21 -0500

    jumbotron minor layout edit

--- src/components/Jumbotron/index.js
+++ src/components/Jumbotron/index.js
@@ -4,7 +4,7 @@ import React from "react";
 function Jumbotron() {
     return (
         <>
-            <section className="m-5 h-screen">
+            <section className="mx-5 py-10 h-screen">
                 <div className="">
                     <div>
                         <h1 className="font-fig text-orange-400 text-md">Hey! My name's</h1>
