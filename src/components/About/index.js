f41e5381bfb61c0db5e220bf9c00ecbeebb5204a
gordo <rogelioalbertopedraza@gmail.com>
2022-08-10 08:32:58 -0500

    completed about section

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -8,7 +8,18 @@ function About() {
                 <div className='h-px w-full bg-stone-400'></div>
             </div>
             <div className='text-stone-400 my-5'>
-                <p>Hello! My name is Rogelio and I </p>
+                <p>Hello! My name is Rogelio and I like creating web applications.
+                    I've always been interested in coding, even when I didn't know much about it.
+                    I had always struggled to find a career I could genuinely be interested in and also be proficient at.
+                    Being the most tech savvy person in my family, it seems as though this path was always hidden in plain sight.
+                    </p>
+            </div>
+            <div className="text-stone-400 my-5">
+                <p>Although I only started to learn coding seriously in February of 2022, I can remember writing
+                    my first lines of HTML and CSS a few years back when I used to run an ebay store.
+                    I'd import custom web fonts and write styled &lt;h1&gt; and &lt;p&gt; tags for item descriptions.
+                    Today, I can create <span className='text-white'>MERN</span> stack applications suited for almost any function using current in-demand technologies.
+                </p>
             </div>
         </section>
     )
