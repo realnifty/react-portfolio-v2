6e143dd859cb204a1374ef7b4fbb67069cca143f
gordo <rogelioalbertopedraza@gmail.com>
2022-08-12 22:06:25 -0500

    adjusted jumbortron font

--- src/components/Jumbotron/index.js
+++ src/components/Jumbotron/index.js
@@ -10,13 +10,13 @@ function Jumbotron() {
                         <h1 className="font-fig text-orange-400 text-md">Hey! My name's</h1>
                     </div>
                     <div>
-                        <h1 className="font-readex text-white text-3xl">Rogelio Pedraza</h1>
+                        <h1 className="font-readex text-white text-4xl my-2">Rogelio Pedraza</h1>
                     </div>
                     <div>
-                        <h1 className="font-fig text-orange-400 mb-5">I'm a full stack coder.</h1>
+                        <h1 className="font-readex text-orange-400 mb-5 text-3xl">I'm a full stack coder.</h1>
                     </div>
                     <div>
-                        <h1 className="font-fig text-stone-400 text-sm">I specialize in both front/back-end software applications, primarily on the web. I'm alway eager to learn new technologies and am currently focused on refining my expertise on the ones I have studied thus far.</h1>
+                        <h1 className="font-fig text-stone-400">I specialize in both front/back-end software applications, primarily on the web. I'm alway eager to learn new technologies and am currently focused on refining my expertise on the ones I have studied thus far.</h1>
                     </div>
                 </div>
             </section>
